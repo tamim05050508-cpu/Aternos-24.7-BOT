7f5045e52c77a47517ca06c8fc0daf714c2db92f
tamim05050508-cpu <tamim05050508@gmail.com>
2026-08-26 22:14:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mineflayer = require('mineflayer');
 
-const serverHost = process.env.SERVER_HOST || 'DOOMS_DAY_REBORN.aternos.me';
-const serverPort = parseInt(process.env.SERVER_PORT || '59173', 10);
+const serverHost = process.env.SERVER_HOST || 'NOXSMP767.aternos.me';
+const serverPort = parseInt(process.env.SERVER_PORT || '18544', 10);
 const botUsername = process.env.BOT_USERNAME || '247_Monitor';
 const minecraftVersion = process.env.MC_VERSION || false;
 const reconnectInterval = parseInt(process.env.RECONNECT_INTERVAL_MS || '40000', 10);
